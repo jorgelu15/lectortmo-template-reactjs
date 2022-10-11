ab8c5b52c23663c8114bee0b61e3a2400554a3b4
Jorge Luis Guardo Romero <deliarosaro112@gmail.com>
2022-10-11 13:34:49 -0500

    card componetizados

--- lectotmo/src/components/MangasFounded.js
+++ lectotmo/src/components/MangasFounded.js
@@ -1,13 +1,13 @@
-import CardFounded from "./Cards/CardFounded";
+import Card from "./Cards/Card";
 import { Mangas } from "../data/Mangas";
 const MangasFounded = () => {
   return (
     <main className="main-founded">
-      <section class="mangasFounded-list">
-        <div class="list-mangasFounded">
-          <div class="list-mangasFounded__list">
+      <section class="hottest">
+        <div class="list-hottest">
+          <div class="list-hottest__list">
             {Mangas.map((manga, idx) => (
-              <CardFounded key={idx} manga={manga} />
+              <Card key={idx} manga={manga} />
             ))}
           </div>
           <a className="btn-see-more">Mostrar más</a>
