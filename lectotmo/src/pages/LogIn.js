38cdcac58510eede643fed521ddc16e7925d0cba
Rous <rosemberc.g.02@gmail.com>
2022-10-12 11:27:08 -0500

    ultimos errores

--- lectotmo/src/pages/LogIn.js
+++ lectotmo/src/pages/LogIn.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import SignIn from '../components/auth/SignIn'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 const LogIn = () => {
   return (
