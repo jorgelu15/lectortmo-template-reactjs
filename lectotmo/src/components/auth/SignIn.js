830f6f391aaf4e961476aa6ad18dfb440382e752
Rous <rosemberc.g.02@gmail.com>
2022-10-12 11:14:15 -0500

    login witch state

--- lectotmo/src/components/auth/SignIn.js
+++ lectotmo/src/components/auth/SignIn.js
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Routers from '../../helper/Routers'
 
 const SignIn = () => {
+  const [datos, setDatos] = useState({
+    mail: '',
+    password: ''
+  })
+  
+  const { mail, password } = datos;
+
+  const onchange = (e)=>{
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value
+    })
+  }
+
+
+  const onsubmit = (e)=>{
+    e.preventDefault()
+  }
   return (
     <>
       <div className='content-login'>
         <div className='content-form'>
-          <form>
-            <p>TUMANGAONLINE</p>
-            <p>Acceder</p>
-            <p>Ir a Lector Manga</p>
-            <div className='inputs'>
-              <div className='input'>
-                <label htmlFor='mail'>Email</label>
-                <input type='mail' name='mail' id='mail' placeholder='Correo electronico' />
-              </div>
-            </div>
+          <form onSubmit={onsubmit}>
+            <p className='logo'>tu<b>manga</b><i>online</i></p>
+            <p className='acceder'>Acceder</p>
+            <Link to={Routers.home}>Ir a Lector Manga</Link>
             <div className='input'>
-              <label htmlFor='password'>Email</label>
-              <input type='password' name='password' id='password' placeholder='Contraseña' />
+              <label htmlFor='mail'>Email</label>
+              <input
+                type='mail'
+                name='mail'
+                id='mail'
+                placeholder='Correo electronico'
+                value={mail}
+                onChange={onchange}
+              />
             </div>
-            <a>¿Olvidaste la Contraseña?</a>
+
+            <div className='input'>
+              <label htmlFor='password'>Contraseña</label>
+              <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Contraseña'
+                value={password}
+                onChange={onchange}
+              />
+            </div>
+            <a className='recuperar-contraseña' href='/'>¿Olvidaste la Contraseña?</a>
             <div className='link-footer'>
-              <a>Crear cuenta</a>
-              <input type='submit' value='Acceder'/>
+              <Link to={Routers.logUp} className='logup'>Crear cuenta</Link>
+              <input type='submit' value='Acceder' />
             </div>
           </form>
         </div>
